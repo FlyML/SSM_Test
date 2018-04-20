c66cdbfd100cf68cb9772acd07f75d0f8be18038
Mr.wu <Mr.wu@ML>
2018-04-20 21:34:43 +0800

    添加了一个Student实体类

---
+++ git-demo01/src/main/java/com/ml/entity/Student.java
@@ -0,0 +1,36 @@
+package com.ml.entity;
+
+import java.sql.Date;
+
+public class Student {
+	private int id;
+	private String name;
+	private String sex;
+	private Date brithday;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public Date getBrithday() {
+		return brithday;
+	}
+	public void setBrithday(Date brithday) {
+		this.brithday = brithday;
+	}
+	
+	
+}
